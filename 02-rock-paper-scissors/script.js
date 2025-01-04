f808ff96793476e636b1d6a2bfd2f16120357224
Sa Ziyang <ziyang.sa@gmail.com>
2025-01-04 23:03:36 +0800

    add everythingit status

--- 02-rock-paper-scissors/script.js
+++ 02-rock-paper-scissors/script.js
@@ -13,6 +13,53 @@ function getComputerChoice(){
     }
 }
 
-getComputerChoice()
-alert(Robot)
+let Person
+let person
+let apple=1;
+
+function getHumanChoice(){
+    let Person = prompt(`Round ${apple} \nPlease enter \"rock\", \"paper\" or \"scissors\":`)
+    person=Person.toLowerCase()
+}
+
+let humanScore=0
+let computerScore=0
+
+function playRound(){
+    getComputerChoice()
+    getHumanChoice()
+    if (person===Robot){
+        alert(`Round ${apple} \nComputer and you both choose "${person}" so we draw.`)
+        alert(`Round ${apple} \n\nYour score: ${humanScore}\nComputer score: ${computerScore}`)
+    } else if ((person=="scissors" && Robot=="paper")||(person=="paper" && Robot=="rock")||(person=="rock" && Robot=="scissors")) {
+        alert(`Round ${apple} \nYou choose "${person}. Computer choose "${Robot}". You win!!`)
+        humanScore=humanScore+1
+        alert(`Round ${apple} \n\nYour score: ${humanScore}\nComputer score: ${computerScore}`)
+    } else if ((person=="paper" && Robot=="scissors")||(person=="rock" && Robot=="paper")||(person=="scissors" && Robot=="rock")) {
+        alert(`Round ${apple} \nYou choose "${person}. Computer choose "${Robot}". You lose.`)
+        computerScore=computerScore+1
+        alert(`Round ${apple} \n\nYour score: ${humanScore}\nComputer score: ${computerScore}`)
+    } else {
+        alert ("Please only enter \"rock\", \"paper\" or \"scissors\":")
+        playRound()
+    }
+}
+
+alert ("You will be playing 5 games of Rock Paper Scissors with the computer. GOOD LUCK!!!")
+
+for (let i=1; i<=5;i++) {
+    playRound()
+    apple++
+}
+
+if (humanScore<computerScore) {
+    alert (`Final round: \n\nYour score: ${humanScore}\nComputer score: ${computerScore} \n\n You lose.`)
+} else if (humanScore>computerScore) {
+    alert (`Final round: \n\nYour score: ${humanScore}\nComputer score: ${computerScore} \n\n You win!!`)
+} else {
+    alert (`Final round: \n\nYour score: ${humanScore}\nComputer score: ${computerScore} \n\n It's a draw.`)
+}
+
+
+
 
