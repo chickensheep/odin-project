cb1e823677c1ab2c4226c43b27985c2cd5be703a
Sa Ziyang <ziyang.sa@gmail.com>
2025-01-08 15:51:55 +0800

    add UI and color

--- 02-rock-paper-scissors/script.js
+++ 02-rock-paper-scissors/script.js
@@ -13,52 +13,208 @@ function getComputerChoice(){
     }
 }
 
-let Person
+
 let person
 let apple=1;
 
-function getHumanChoice(){
-    let Person = prompt(`Round ${apple} \nPlease enter \"rock\", \"paper\" or \"scissors\":`)
-    person=Person.toLowerCase()
-}
+// function getHumanChoice(){
+//     let Person = prompt(`Round ${apple} \nPlease enter \"rock\", \"paper\" or \"scissors\":`)
+//     person=Person.toLowerCase()
+// }
 
 let humanScore=0
 let computerScore=0
 
 function playRound(){
     getComputerChoice()
-    getHumanChoice()
+    // getHumanChoice()
     if (person===Robot){
-        alert(`Round ${apple} \nComputer and you both choose "${person}" so we draw. :o`)
-        alert(`Round ${apple} \n\nYour score: ${humanScore}\nComputer score: ${computerScore}`)
+        result.textContent=`Round ${apple} \nComputer and you both choose "${person}" so we draw. :o`
+        score.textContent=` Round ${apple} \n\nYour score: ${humanScore}\nComputer score: ${computerScore}`
+        apple++
     } else if ((person=="scissors" && Robot=="paper")||(person=="paper" && Robot=="rock")||(person=="rock" && Robot=="scissors")) {
-        alert(`Round ${apple} \nYou choose "${person}". Computer choose "${Robot}". You win!! :D`)
         humanScore=humanScore+1
-        alert(`Round ${apple} \n\nYour score: ${humanScore}\nComputer score: ${computerScore}`)
+        result.textContent=`Round ${apple} \nYou choose "${person}". Computer choose "${Robot}". You win!! :D`
+        score.textContent=`Round ${apple} \n\nYour score: ${humanScore}\nComputer score: ${computerScore}`
+        apple++
     } else if ((person=="paper" && Robot=="scissors")||(person=="rock" && Robot=="paper")||(person=="scissors" && Robot=="rock")) {
-        alert(`Round ${apple} \nYou choose "${person}". Computer choose "${Robot}". You lose. D:`)
-        computerScore=computerScore+1
-        alert(`Round ${apple} \n\nYour score: ${humanScore}\nComputer score: ${computerScore}`)
+        computerScore=computerScore+1        
+        result.textContent=`Round ${apple} \nYou choose "${person}". Computer choose "${Robot}". You lose. D:`
+        score.textContent=`Round ${apple} \n\nYour score: ${humanScore}\nComputer score: ${computerScore}`
+        apple++
+    } 
+    // else {
+    //     alert ("Please only enter \"rock\", \"paper\" or \"scissors\":")
+    //     playRound()
+    // }
+}
+
+// alert ("You will be playing 5 games of Rock Paper Scissors with the computer. GOOD LUCK!!!")
+
+// for (let i=1; i<=5;i++) {
+//     playRound()
+//     apple++
+// }
+
+function abc(){
+    if (humanScore<computerScore) {
+        finalResult.textContent=`Final result: \n\nYour score: ${humanScore}\nComputer score: ${computerScore} \n\n You lose. D:`
+    } else if (humanScore>computerScore) {
+        finalResult.textContent=`Final result: \n\nYour score: ${humanScore}\nComputer score: ${computerScore} \n\n You win!! :D`
     } else {
-        alert ("Please only enter \"rock\", \"paper\" or \"scissors\":")
-        playRound()
+        finalResult.textContent= `Final result: \n\nYour score: ${humanScore}\nComputer score: ${computerScore} \n\n It's a draw. :o`
     }
 }
 
-alert ("You will be playing 5 games of Rock Paper Scissors with the computer. GOOD LUCK!!!")
 
-for (let i=1; i<=5;i++) {
-    playRound()
-    apple++
-}
+const content=document.querySelector("div")
+const title=document.querySelector("h1")
+const choice=document.querySelector(".choice")
+const result=document.querySelector(".result")
+const score=document.querySelector(".score")
+const rock = document.createElement("button")
+const paper = document.createElement("button")
+const scissors = document.createElement("button")
 
-if (humanScore<computerScore) {
-    alert (`Final round: \n\nYour score: ${humanScore}\nComputer score: ${computerScore} \n\n You lose. D:`)
-} else if (humanScore>computerScore) {
-    alert (`Final round: \n\nYour score: ${humanScore}\nComputer score: ${computerScore} \n\n You win!! :D`)
-} else {
-    alert (`Final round: \n\nYour score: ${humanScore}\nComputer score: ${computerScore} \n\n It's a draw. :o`)
-}
+rock.className="rock"
+paper.className="paper"
+scissors.className="scissors"
+
+choice.appendChild(rock)
+choice.appendChild(paper)
+choice.appendChild(scissors)
+
+rock.textContent="Rock"
+paper.textContent="Paper"
+scissors.textContent="Scissors"
+
+
+rock.addEventListener('click', ()=>{
+    if (apple<=4){
+        person="rock"
+        playRound(); 
+    } else if (apple==5) {
+        person="rock"
+        playRound(); 
+        const button=document.createElement("button")
+        const line=document.createElement("hr")
+        content.appendChild(line)
+        content.appendChild(button)
+        button.textContent="Result"
+        button.addEventListener("click",()=>{
+            const finalResult=document.createElement("p");
+            content.appendChild(finalResult);
+            if (humanScore<computerScore) {
+                finalResult.textContent=`Final result: You lose. D:`
+            } else if (humanScore>computerScore) {
+                finalResult.textContent=`Final result: You win!! :D`
+            } else {
+                finalResult.textContent= `Final result: It's a draw. :o`
+            }
+            const returnMain=document.createElement("a");
+            content.appendChild(returnMain);
+            let link=document.createTextNode("Return to Main Project Page")
+            returnMain.appendChild(link)
+            returnMain.href="../index.html"
+            button.remove();
+            
+        })
+        
+    } else {
+        alert("GAME OVER!!! stop pressing me (ò_ó)")
+    }
+    
+})
+
+paper.addEventListener('click', ()=>{
+    if (apple<=4){
+        person="paper"
+        playRound(); 
+    } else if (apple==5) {
+        person="rock"
+        playRound(); 
+        const button=document.createElement("button")
+        const line=document.createElement("hr")
+        content.appendChild(line)
+        content.appendChild(button)
+        button.textContent="Result"
+        button.addEventListener("click",()=>{
+            const finalResult=document.createElement("p");
+            content.appendChild(finalResult);
+            if (humanScore<computerScore) {
+                finalResult.textContent=`Final result: You lose. D:`
+            } else if (humanScore>computerScore) {
+                finalResult.textContent=`Final result: You win!! :D`
+            } else {
+                finalResult.textContent= `Final result: It's a draw. :o`
+            }
+            const returnMain=document.createElement("a");
+            content.appendChild(returnMain);
+            let link=document.createTextNode("Return to Main Project Page")
+            returnMain.appendChild(link)
+            returnMain.href="../index.html"
+            button.remove();
+            
+        })
+        
+    } else {
+        alert("GAME OVER!!! stop pressing me (ò_ó)")
+    }
+    
+})
+
+scissors.addEventListener('click', ()=>{
+    if (apple<=4){
+        person="scissors"
+        playRound(); 
+    } else if (apple==5) {
+        person="rock"
+        playRound(); 
+        const button=document.createElement("button")
+        const line=document.createElement("hr")
+        content.appendChild(line)
+        content.appendChild(button)
+        button.textContent="Result"
+        button.addEventListener("click",()=>{
+            const finalResult=document.createElement("p");
+            content.appendChild(finalResult);
+            if (humanScore<computerScore) {
+                finalResult.textContent=`Final result: You lose. D:`
+            } else if (humanScore>computerScore) {
+                finalResult.textContent=`Final result: You win!! :D`
+            } else {
+                finalResult.textContent= `Final result: It's a draw. :o`
+            }
+            const returnMain=document.createElement("a");
+            content.appendChild(returnMain);
+            let link=document.createTextNode("Return to Main Project Page")
+            returnMain.appendChild(link)
+            returnMain.href="../index.html"
+            button.remove();
+        })
+        
+    } else {
+        alert("GAME OVER!!! stop pressing me (ò_ó)")
+    }
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
